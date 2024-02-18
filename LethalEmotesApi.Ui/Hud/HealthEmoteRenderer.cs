03a8c0b490ab34b8cbc16022d223769132df4fcd
Rune580 <runemaster580@gmail.com>
2024-02-17 22:23:28 -0500

    pressed the "optimize code" button in unity

--- LethalEmotesApi.Ui/Hud/HealthEmoteRenderer.cs
+++ LethalEmotesApi.Ui/Hud/HealthEmoteRenderer.cs
@@ -9,38 +9,51 @@ public class HealthEmoteRenderer : MonoBehaviour
     public SkinnedMeshRenderer? emoteSkinnedMeshRenderer;
     public Material? material;
     public RenderTexture? targetRenderTexture;
-    
+
+    private CommandBuffer? _cmdBuf;
+    private readonly Matrix4x4 _projMat = Matrix4x4.Perspective(60f, 1, 0.3f, 15f);
+
+    // Apparently this is more performant than:
+    //     var camTransform = transform;
+    //     var pos = camTransform.position;
+    //     var lookMat = Matrix4x4.LookAt(pos, pos + camTransform.forward, camTransform.up);
+    //     var scaleMat = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, 1, -1));
+    //     var viewMat = scaleMat * lookMat.inverse;
+    private Matrix4x4 GetViewMatrix()
+    {
+        var camTransform = transform;
+        var viewMat = Matrix4x4.Rotate(Quaternion.Inverse(camTransform.rotation)) *
+                      Matrix4x4.Translate(-camTransform.position);
+
+        if (SystemInfo.usesReversedZBuffer)
+        {
+            viewMat.m20 = -viewMat.m20;
+            viewMat.m21 = -viewMat.m21;
+            viewMat.m22 = -viewMat.m22;
+            viewMat.m23 = -viewMat.m23;
+        }
+
+        return viewMat;
+    }
+
+    private void Start()
+    {
+        _cmdBuf = new CommandBuffer();
+    }
+
     private void LateUpdate()
     {
-        if (emoteSkinnedMeshRenderer is null || material is null || targetRenderTexture is null)
-            return;
-        
-        var projMat = Matrix4x4.Perspective(60f, 1, 0.3f, 15f);
+        _cmdBuf!.Clear();
+        _cmdBuf.SetViewProjectionMatrices(GetViewMatrix(), _projMat);
+        _cmdBuf.DrawRenderer(emoteSkinnedMeshRenderer, material);
 
-        var camTransform = transform;
-        var pos = camTransform.position;
-
-        var lookMat = Matrix4x4.LookAt(pos, pos + camTransform.forward, camTransform.up);
-        var scaleMat = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, 1, -1));
-
-        var viewMat = scaleMat * lookMat.inverse;
-        
-        var buf = new CommandBuffer();
-        
-        buf.Clear();
-        
-        buf.SetViewProjectionMatrices(viewMat, projMat);
-        
-        buf.DrawRenderer(emoteSkinnedMeshRenderer, material);
-
-        var prevRenderTexture = RenderTexture.active;
-        
         Graphics.SetRenderTarget(targetRenderTexture);
-        
         GL.Clear(false, true, Color.green);
-        
-        Graphics.ExecuteCommandBuffer(buf);
+        Graphics.ExecuteCommandBuffer(_cmdBuf);
+    }
 
-        Graphics.SetRenderTarget(prevRenderTexture);
+    private void OnDestroy()
+    {
+        _cmdBuf?.Dispose();
     }
 }
