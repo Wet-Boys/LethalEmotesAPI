658bbb570a61eae165d321c0d73aab5b4b448bc5
Nunchuk/ToasterOven <49164897+ToastedOven@users.noreply.github.com>
2024-04-18 18:24:42 -0400

    Create NearbyEmoteChecker.cs

---
+++ CustomEmotesAPI/Utils/NearbyEmoteChecker.cs
@@ -0,0 +1,34 @@
+﻿using EmotesAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LethalEmotesAPI.Utils
+{
+    public struct NearbyEmote(float distance, string playerName, string emoteKey)
+    {
+        float distance = distance;
+        string playerName = playerName;
+        string emoteKey = emoteKey;
+    }
+    public class NearbyEmoteChecker
+    {
+        public static NearbyEmote[] GetNearbyEmotes(float maxDistance)
+        {
+            List<NearbyEmote> nearbyEmotes = new List<NearbyEmote>();
+            foreach (var item in BoneMapper.allMappers)
+            {
+                if (!item.local && item.currentClip is not null)
+                {
+                    float dist = Vector3.Distance(item.transform.position, CustomEmotesAPI.localMapper.transform.position);
+                    if (dist <= maxDistance)
+                    {
+                        nearbyEmotes.Add(new NearbyEmote(dist, item.playerController.playerUsername, item.currentClip.customInternalName));
+                    }
+                }
+            }
+            return nearbyEmotes.ToArray();
+        }
+    }
+}
