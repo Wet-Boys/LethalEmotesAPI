3ba054cd12abff9119b63971fe363257814e3a0a
Nunchuk/ToasterOven <49164897+ToastedOven@users.noreply.github.com>
2024-04-22 20:35:47 -0400

    Changelog

--- CustomEmotesAPI/CustomEmotesAPI.cs
+++ CustomEmotesAPI/CustomEmotesAPI.cs
@@ -49,7 +49,7 @@ namespace EmotesAPI
 
         public const string PluginName = "Custom Emotes API";
 
-        public const string VERSION = "1.10.1";
+        public const string VERSION = "1.11.0";
         public struct NameTokenWithSprite
         {
             public string nameToken;
