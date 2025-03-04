9c0f91f6c5470239986e88e7648b1659aa582d5a
Nunchuk <49164897+ToastedOven@users.noreply.github.com>
2025-03-04 15:08:29 -0500

    Update CustomEmotesAPI.cs

--- CustomEmotesAPI/CustomEmotesAPI.cs
+++ CustomEmotesAPI/CustomEmotesAPI.cs
@@ -51,7 +51,7 @@ namespace EmotesAPI
 
         public const string PluginName = "Custom Emotes API";
 
-        public const string VERSION = "1.15.11";
+        public const string VERSION = "1.15.12";
         public struct NameTokenWithSprite
         {
             public string nameToken;
